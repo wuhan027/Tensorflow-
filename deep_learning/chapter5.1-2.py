25744c5db759f89f9e7d35ad22e2d9d37f012ece
fengbin <fengbin.liu@taou.com>
2020-04-21 09:50:58 +0800

    add

---
+++ deep_learning/chapter5.1-2.py
@@ -0,0 +1,211 @@
+import numpy as np
+
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
+
+def smooth(loss, cur_loss):
+    return loss * 0.999 + cur_loss * 0.001
+
+def print_sample(sample_ix, ix_to_char):
+    txt = ''.join(ix_to_char[ix] for ix in sample_ix)
+    txt = txt[0].upper() + txt[1:]  # capitalize first character
+    print ('%s' % (txt, ), end='')
+
+def get_initial_loss(vocab_size, seq_length):
+    return -np.log(1.0/vocab_size)*seq_length
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
+
+def initialize_parameters(n_a, n_x, n_y):
+    """
+    Initialize parameters with small random values
+
+    Returns:
+    parameters -- python dictionary containing:
+                        Wax -- Weight matrix multiplying the input, numpy array of shape (n_a, n_x)
+                        Waa -- Weight matrix multiplying the hidden state, numpy array of shape (n_a, n_a)
+                        Wya -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+                        b --  Bias, numpy array of shape (n_a, 1)
+                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+    """
+    np.random.seed(1)
+    Wax = np.random.randn(n_a, n_x)*0.01 # input to hidden
+    Waa = np.random.randn(n_a, n_a)*0.01 # hidden to hidden
+    Wya = np.random.randn(n_y, n_a)*0.01 # hidden to output
+    b = np.zeros((n_a, 1)) # hidden bias
+    by = np.zeros((n_y, 1)) # output bias
+
+    parameters = {"Wax": Wax, "Waa": Waa, "Wya": Wya, "b": b,"by": by}
+
+    return parameters
+
+def rnn_step_forward(parameters, a_prev, x):
+
+    Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
+    a_next = np.tanh(np.dot(Wax, x) + np.dot(Waa, a_prev) + b) # hidden state
+    p_t = softmax(np.dot(Wya, a_next) + by) # unnormalized log probabilities for next chars # probabilities for next chars
+
+    return a_next, p_t
+
+def rnn_step_backward(dy, gradients, parameters, x, a, a_prev):
+
+    gradients['dWya'] += np.dot(dy, a.T)
+    gradients['dby'] += dy
+    da = np.dot(parameters['Wya'].T, dy) + gradients['da_next'] # backprop into h
+    daraw = (1 - a * a) * da # backprop through tanh nonlinearity
+    gradients['db'] += daraw
+    gradients['dWax'] += np.dot(daraw, x.T)
+    gradients['dWaa'] += np.dot(daraw, a_prev.T)
+    gradients['da_next'] = np.dot(parameters['Waa'].T, daraw)
+    return gradients
+
+def update_parameters(parameters, gradients, lr):
+
+    parameters['Wax'] += -lr * gradients['dWax']
+    parameters['Waa'] += -lr * gradients['dWaa']
+    parameters['Wya'] += -lr * gradients['dWya']
+    parameters['b']  += -lr * gradients['db']
+    parameters['by']  += -lr * gradients['dby']
+    return parameters
+
+def rnn_forward(X, Y, a0, parameters, vocab_size = 27):
+
+    # Initialize x, a and y_hat as empty dictionaries
+    x, a, y_hat = {}, {}, {}
+
+    a[-1] = np.copy(a0)
+
+    # initialize your loss to 0
+    loss = 0
+
+    for t in range(len(X)):
+
+        # Set x[t] to be the one-hot vector representation of the t'th character in X.
+        # if X[t] == None, we just have x[t]=0. This is used to set the input for the first timestep to the zero vector.
+        x[t] = np.zeros((vocab_size,1))
+        if (X[t] != None):
+            x[t][X[t]] = 1
+
+        # Run one step forward of the RNN
+        a[t], y_hat[t] = rnn_step_forward(parameters, a[t-1], x[t])
+
+        # Update the loss by substracting the cross-entropy term of this time-step from it.
+        loss -= np.log(y_hat[t][Y[t],0])
+
+    cache = (y_hat, a, x)
+
+    return loss, cache
+
+
+def rnn_backward(X, Y, parameters, cache):
+    # Initialize gradients as an empty dictionary
+    gradients = {}
+
+    # Retrieve from cache and parameters
+    (y_hat, a, x) = cache
+    Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
+
+    # each one should be initialized to zeros of the same dimension as its corresponding parameter
+    gradients['dWax'], gradients['dWaa'], gradients['dWya'] = np.zeros_like(Wax), np.zeros_like(Waa), np.zeros_like(Wya)
+    gradients['db'], gradients['dby'] = np.zeros_like(b), np.zeros_like(by)
+    gradients['da_next'] = np.zeros_like(a[0])
+
+    ### START CODE HERE ###
+    # Backpropagate through time
+    for t in reversed(range(len(X))):
+        dy = np.copy(y_hat[t])
+        dy[Y[t]] -= 1
+        gradients = rnn_step_backward(dy, gradients, parameters, x[t], a[t], a[t-1])
+    ### END CODE HERE ###
+
+    return gradients, a
+
+
+# GRADED FUNCTION: sample
+def sample(parameters, char_to_ix, seed):
+    """
+    Sample a sequence of characters according to a sequence of probability distributions output of the RNN
+
+    Arguments:
+    parameters -- python dictionary containing the parameters Waa, Wax, Wya, by, and b.
+    char_to_ix -- python dictionary mapping each character to an index.
+    seed -- used for grading purposes. Do not worry about it.
+
+    Returns:
+    indices -- a list of length n containing the indices of the sampled characters.
+    """
+
+    # Retrieve parameters and relevant shapes from "parameters" dictionary
+    Waa, Wax, Wya, by, b = parameters['Waa'], parameters['Wax'], parameters['Wya'], parameters['by'], parameters['b']
+    vocab_size = by.shape[0]
+    n_a = Waa.shape[1]
+
+    ### START CODE HERE ###
+    # Step 1: Create the one-hot vector x for the first character (initializing the sequence generation). (≈1 line)
+    x = np.zeros((vocab_size, 1))
+    # Step 1': Initialize a_prev as zeros (≈1 line)
+    a_prev = np.zeros((n_a, 1))
+
+    # Create an empty list of indices, this is the list which will contain the list of indices of the characters to generate (≈1 line)
+    indices = []
+
+    # Idx is a flag to detect a newline character, we initialize it to -1
+    idx = -1
+
+    # Loop over time-steps t. At each time-step, sample a character from a probability distribution and append
+    # its index to "indices". We'll stop if we reach 50 characters (which should be very unlikely with a well
+    # trained model), which helps debugging and prevents entering an infinite loop.
+    counter = 0
+    newline_character = char_to_ix['\n']
+
+    while (idx != newline_character and counter != 50):
+        # Step 2: Forward propagate x using the equations (1), (2) and (3)
+        a = np.tanh(np.dot(Wax, x) + np.dot(Waa, a_prev) + b)
+        z = np.dot(Wya, a) + by
+        y = softmax(z)
+        print('a=', a.shape, '\tz=', z.shape, '\ty=', y.shape)
+
+        # for grading purposes
+        np.random.seed(counter+seed)
+
+        # Step 3: Sample the index of a character within the vocabulary from the probability distribution y
+        idx = np.random.choice(range(len(y)), p = y.ravel())
+
+        # Append the index to "indices"
+        indices.append(idx)
+
+        # Step 4: Overwrite the input character as the one corresponding to the sampled index.
+        x = np.zeros((vocab_size, 1))
+        x[idx] = 1
+
+        # Update "a_prev" to be "a"
+        a_prev = a
+
+        # for grading purposes
+        seed += 1
+        counter +=1
+
+    ### END CODE HERE ###
+
+    if (counter == 50):
+        indices.append(char_to_ix['\n'])
+
+    return indices
+np.random.seed(2)
+_, n_a = 20, 100
+vocab_size=27
+Wax, Waa, Wya = np.random.randn(n_a, vocab_size), np.random.randn(n_a, n_a), np.random.randn(vocab_size, n_a)
+b, by = np.random.randn(n_a, 1), np.random.randn(vocab_size, 1)
+parameters = {"Wax": Wax, "Waa": Waa, "Wya": Wya, "b": b, "by": by}
+ix_to_char = {0: '\n', 1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j', 11: 'k', 12: 'l', 13: 'm', 14: 'n', 15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't', 21: 'u', 22: 'v', 23: 'w', 24: 'x', 25: 'y', 26: 'z'}
+char_to_ix = {}
+for _k in ix_to_char:
+    char_to_ix[ix_to_char[_k]] = _k
+indices = sample(parameters, char_to_ix, 0)
+print("Sampling:")
+print("list of sampled indices:", indices)
+print("list of sampled characters:", [ix_to_char[i] for i in indices])
