fc9945e0bbd577482efe3aa106fbc35b938a0915
fengbin <liufengbin87@126.com>
2020-04-24 19:40:19 +0800

    add chapter5.2-1.py

---
+++ deep_learning/chapter5.2-1.py
@@ -0,0 +1,238 @@
+import numpy as np
+from keras.models import Model
+from keras.layers import Input, Dense, Reshape, merge
+from keras.layers.embeddings import Embedding
+from keras.preprocessing.sequence import skipgrams
+from keras.preprocessing import sequence
+
+import urllib.request
+import collections
+import os
+import zipfile
+
+import numpy as np
+import tensorflow as tf
+
+window_size = 3
+vector_dim = 300
+epochs = 1000
+
+valid_size = 16     # Random set of words to evaluate similarity on.
+valid_window = 100  # Only pick dev samples in the head of the distribution.
+valid_examples = np.random.choice(valid_window, valid_size, replace=False)
+
+
+def maybe_download(filename, url, expected_bytes):
+    """Download a file if not present, and make sure it's the right size."""
+    if not os.path.exists(filename):
+        filename, _ = urllib.request.urlretrieve(url + filename, filename)
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified', filename)
+    else:
+        print(statinfo.st_size)
+        raise Exception(
+            'Failed to verify ' + filename + '. Can you get to it with a browser?')
+    return filename
+
+
+# Read the data into a list of strings.
+def read_data(filename):
+    """Extract the first file enclosed in a zip file as a list of words."""
+    with zipfile.ZipFile(filename) as f:
+        data = tf.compat.as_str(f.read(f.namelist()[0])).split()
+    return data
+
+
+def build_dataset(words, n_words):
+    """Process raw inputs into a dataset."""
+    count = [['UNK', -1]]
+    count.extend(collections.Counter(words).most_common(n_words - 1))
+    dictionary = dict()
+    for word, _ in count:
+        dictionary[word] = len(dictionary)
+    data = list()
+    unk_count = 0
+    for word in words:
+        if word in dictionary:
+            index = dictionary[word]
+        else:
+            index = 0  # dictionary['UNK']
+            unk_count += 1
+        data.append(index)
+    count[0][1] = unk_count
+    reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return data, count, dictionary, reversed_dictionary
+
+
+def collect_data(vocabulary_size=10000):
+    url = 'http://mattmahoney.net/dc/'
+    filename = maybe_download('text8.zip', url, 31344016)
+    vocabulary = read_data(filename)
+    print(vocabulary[:7])
+    data, count, dictionary, reverse_dictionary = build_dataset(vocabulary,
+                                                                vocabulary_size)
+    del vocabulary  # Hint to reduce memory.
+    return data, count, dictionary, reverse_dictionary
+
+
+class SimilarityCallback:
+    def run_sim(self):
+        for i in range(valid_size):
+            valid_word = reverse_dictionary[valid_examples[i]]
+            top_k = 8  # number of nearest neighbors
+            sim = self._get_sim(valid_examples[i])
+            nearest = (-sim).argsort()[1:top_k + 1]
+            log_str = 'Nearest to %s:' % valid_word
+            for k in range(top_k):
+                close_word = reverse_dictionary[nearest[k]]
+                log_str = '%s %s,' % (log_str, close_word)
+            print(log_str)
+
+    @staticmethod
+    def _get_sim(valid_word_idx):
+        sim = np.zeros((vocab_size,))
+        in_arr1 = np.zeros((1,))
+        in_arr2 = np.zeros((1,))
+        in_arr1[0,] = valid_word_idx
+        for i in range(vocab_size):
+            in_arr2[0,] = i
+            out = validation_model.predict_on_batch([in_arr1, in_arr2])
+            sim[i] = out
+        return sim
+
+
+def read_glove_vecs(glove_file):
+    with open(glove_file, 'r') as f:
+        words = set()
+        word_to_vec_map = {}
+
+        for line in f:
+            line = line.strip().split()
+            curr_word = line[0]
+            words.add(curr_word)
+            word_to_vec_map[curr_word] = np.array(line[1:], dtype=np.float64)
+
+    return words, word_to_vec_map
+
+
+def relu(x):
+    """
+    Compute the relu of x
+
+    Arguments:
+    x -- A scalar or numpy array of any size.
+
+    Return:
+    s -- relu(x)
+    """
+    s = np.maximum(0,x)
+
+    return s
+
+
+def initialize_parameters(vocab_size, n_h):
+    """
+    Arguments:
+    layer_dims -- python array (list) containing the dimensions of each layer in our network
+
+    Returns:
+    parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2":
+                    W1 -- weight matrix of shape (n_h, vocab_size)
+                    b1 -- bias vector of shape (n_h, 1)
+                    W2 -- weight matrix of shape (vocab_size, n_h)
+                    b2 -- bias vector of shape (vocab_size, 1)
+    """
+
+    np.random.seed(3)
+    parameters = {}
+
+    parameters['W1'] = np.random.randn(n_h, vocab_size) / np.sqrt(vocab_size)
+    parameters['b1'] = np.zeros((n_h, 1))
+    parameters['W2'] = np.random.randn(vocab_size, n_h) / np.sqrt(n_h)
+    parameters['b2'] = np.zeros((vocab_size, 1))
+
+    return parameters
+
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+
+# GRADED FUNCTION: cosine_similarity
+def cosine_similarity(u, v):
+    """
+    Cosine similarity reflects the degree of similariy between u and v
+
+    Arguments:
+        u -- a word vector of shape (n,)
+        v -- a word vector of shape (n,)
+
+    Returns:
+        cosine_similarity -- the cosine similarity between u and v defined by the formula above.
+    """
+
+    ### START CODE HERE ###
+    # Compute the dot product between u and v (≈1 line)
+    dot = np.dot(u, v)
+    # Compute the L2 norm of u (≈1 line)
+    norm_u = np.sqrt(np.sum(u**2))
+
+    # Compute the L2 norm of v (≈1 line)
+    norm_v = np.sqrt(np.sum(v**2))
+    # Compute the cosine similarity defined by formula (1) (≈1 line)
+    cosine_similarity = dot / (norm_u * norm_v)
+    ### END CODE HERE ###
+
+    return cosine_similarity
+
+
+# GRADED FUNCTION: complete_analogy
+def complete_analogy(word_a, word_b, word_c, word_to_vec_map):
+    """
+    Performs the word analogy task as explained above: a is to b as c is to ____.
+
+    Arguments:
+    word_a -- a word, string
+    word_b -- a word, string
+    word_c -- a word, string
+    word_to_vec_map -- dictionary that maps words to their corresponding vectors.
+
+    Returns:
+    best_word --  the word such that v_b - v_a is close to v_best_word - v_c, as measured by cosine similarity
+    """
+
+    # convert words to lower case
+    word_a, word_b, word_c = word_a.lower(), word_b.lower(), word_c.lower()
+
+    ### START CODE HERE ###
+    # Get the word embeddings v_a, v_b and v_c (≈1-3 lines)
+    e_a, e_b, e_c = word_to_vec_map[word_a], word_to_vec_map[word_b], word_to_vec_map[word_c]
+    ### END CODE HERE ###
+
+    words = word_to_vec_map.keys()
+    max_cosine_sim = -100              # Initialize max_cosine_sim to a large negative number
+    best_word = None                   # Initialize best_word with None, it will help keep track of the word to output
+
+    # loop over the whole word vector set
+    for w in words:
+        # to avoid best_word being one of the input words, pass on them.
+        if w in [word_a, word_b, word_c] :
+            continue
+
+        ### START CODE HERE ###
+        # Compute cosine similarity between the vector (e_b - e_a) and the vector ((w's vector representation) - e_c)  (≈1 line)
+        cosine_sim = cosine_similarity(e_b - e_a, word_to_vec_map[w] - e_c)
+
+        # If the cosine_sim is more than the max_cosine_sim seen so far,
+            # then: set the new max_cosine_sim to the current cosine_sim and the best_word to the current word (≈3 lines)
+        if cosine_sim > max_cosine_sim:
+            max_cosine_sim = cosine_sim
+            best_word = w
+        ### END CODE HERE ###
+
+    return best_word
+
+
