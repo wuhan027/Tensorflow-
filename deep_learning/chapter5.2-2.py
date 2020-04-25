422959e75c31d02fc744651910cedab868b99857
fengbin <fengbin.liu@taou.com>
2020-04-25 22:09:46 +0800

    add chapter5.2-2

---
+++ deep_learning/chapter5.2-2.py
@@ -0,0 +1,394 @@
+'''
+What you should remember:
+- If you have an NLP task where the training set is small, using word embeddings can help your algorithm significantly. Word embeddings allow your model to work on words in the test set that may not even have appeared in your training set.
+- Training sequence models in Keras (and in most other deep learning frameworks) requires a few important details:
+- To use mini-batches, the sequences need to be padded so that all the examples in a mini-batch have the same length.
+- An Embedding() layer can be initialized with pretrained values. These values can be either fixed or trained further on your dataset. If however your labeled dataset is small, it’s usually not worth trying to train a large pre-trained set of embeddings.
+- LSTM() has a flag called return_sequences to decide if you would like to return every hidden states or only the last one.
+- You can use Dropout() right after LSTM() to regularize your network.
+————————————————
+'''
+import csv
+import numpy as np
+import emoji
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+
+
+def read_glove_vecs(glove_file):
+    with open(glove_file, 'r') as f:
+        words = set()
+        word_to_vec_map = {}
+        for line in f:
+            line = line.strip().split()
+            curr_word = line[0]
+            words.add(curr_word)
+            word_to_vec_map[curr_word] = np.array(line[1:], dtype=np.float64)
+
+        i = 1
+        words_to_index = {}
+        index_to_words = {}
+        for w in sorted(words):
+            words_to_index[w] = i
+            index_to_words[i] = w
+            i = i + 1
+    return words_to_index, index_to_words, word_to_vec_map
+
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+
+def read_csv(filename = 'data/emojify_data.csv'):
+    phrase = []
+    emoji = []
+
+    with open (filename) as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+
+        for row in csvReader:
+            phrase.append(row[0])
+            emoji.append(row[1])
+
+    X = np.asarray(phrase)
+    Y = np.asarray(emoji, dtype=int)
+
+    return X, Y
+
+
+def convert_to_one_hot(Y, C):
+    Y = np.eye(C)[Y.reshape(-1)]
+    return Y
+
+
+emoji_dictionary = {"0": "\u2764\uFE0F",    # :heart: prints a black instead of red heart depending on the font
+                    "1": ":baseball:",
+                    "2": ":smile:",
+                    "3": ":disappointed:",
+                    "4": ":fork_and_knife:"}
+
+
+def label_to_emoji(label):
+    """
+    Converts a label (int or string) into the corresponding emoji code (string) ready to be printed
+    """
+    return emoji.emojize(emoji_dictionary[str(label)], use_aliases=True)
+
+
+def print_predictions(X, pred):
+    print()
+    for i in range(X.shape[0]):
+        print(X[i], label_to_emoji(int(pred[i])))
+
+
+def plot_confusion_matrix(y_actu, y_pred, title='Confusion matrix', cmap=plt.cm.gray_r):
+
+    df_confusion = pd.crosstab(y_actu, y_pred.reshape(y_pred.shape[0],), rownames=['Actual'], colnames=['Predicted'], margins=True)
+
+    df_conf_norm = df_confusion / df_confusion.sum(axis=1)
+
+    plt.matshow(df_confusion, cmap=cmap) # imshow
+    #plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(df_confusion.columns))
+    plt.xticks(tick_marks, df_confusion.columns, rotation=45)
+    plt.yticks(tick_marks, df_confusion.index)
+    #plt.tight_layout()
+    plt.ylabel(df_confusion.index.name)
+    plt.xlabel(df_confusion.columns.name)
+
+
+def predict(X, Y, W, b, word_to_vec_map):
+    """
+    Given X (sentences) and Y (emoji indices), predict emojis and compute the accuracy of your model over the given set.
+
+    Arguments:
+    X -- input data containing sentences, numpy array of shape (m, None)
+    Y -- labels, containing index of the label emoji, numpy array of shape (m, 1)
+
+    Returns:
+    pred -- numpy array of shape (m, 1) with your predictions
+    """
+    m = X.shape[0]
+    pred = np.zeros((m, 1))
+
+    for j in range(m):                       # Loop over training examples
+
+        # Split jth test example (sentence) into list of lower case words
+        words = X[j].lower().split()
+
+        # Average words' vectors
+        avg = np.zeros((50,))
+        for w in words:
+            avg += word_to_vec_map[w]
+        avg = avg/len(words)
+
+        # Forward propagation
+        Z = np.dot(W, avg) + b
+        A = softmax(Z)
+        pred[j] = np.argmax(A)
+
+    print("Accuracy: "  + str(np.mean((pred[:] == Y.reshape(Y.shape[0],1)[:]))))
+
+    return pred
+
+
+X_train, Y_train = read_csv('data/train_emoji.csv')
+X_test, Y_test = read_csv('data/tesss.csv')
+maxLen = len(max(X_train, key=len).split())
+
+Y_oh_train = convert_to_one_hot(Y_train, C = 5)
+Y_oh_test = convert_to_one_hot(Y_test, C = 5)
+
+
+# GRADED FUNCTION: sentence_to_avg
+
+def sentence_to_avg(sentence, word_to_vec_map):
+    """
+    Converts a sentence (string) into a list of words (strings). Extracts the GloVe representation of each word
+    and averages its value into a single vector encoding the meaning of the sentence.
+
+    Arguments:
+    sentence -- string, one training example from X
+    word_to_vec_map -- dictionary mapping every word in a vocabulary into its 50-dimensional vector representation
+
+    Returns:
+    avg -- average vector encoding information about the sentence, numpy-array of shape (50,)
+    """
+
+    ### START CODE HERE ###
+    # Step 1: Split sentence into list of lower case words (鈮?1 line)
+    words = sentence.lower().split()
+
+    # Initialize the average word vector, should have the same shape as your word vectors.
+    avg = np.zeros((50,))
+
+    # Step 2: average the word vectors. You can loop over the words in the list "words".
+    for w in words:
+        avg += word_to_vec_map[w]
+    avg = avg/len(words)
+
+    ### END CODE HERE ###
+
+    return avg
+
+
+# GRADED FUNCTION: model
+
+def model(X, Y, word_to_vec_map, learning_rate = 0.01, num_iterations = 400):
+    """
+    Model to train word vector representations in numpy.
+
+    Arguments:
+    X -- input data, numpy array of sentences as strings, of shape (m, 1)
+    Y -- labels, numpy array of integers between 0 and 7, numpy-array of shape (m, 1)
+    word_to_vec_map -- dictionary mapping every word in a vocabulary into its 50-dimensional vector representation
+    learning_rate -- learning_rate for the stochastic gradient descent algorithm
+    num_iterations -- number of iterations
+
+    Returns:
+    pred -- vector of predictions, numpy-array of shape (m, 1)
+    W -- weight matrix of the softmax layer, of shape (n_y, n_h)
+    b -- bias of the softmax layer, of shape (n_y,)
+    """
+
+    np.random.seed(1)
+
+    # Define number of training examples
+    m = Y.shape[0]                          # number of training examples
+    n_y = 5                                 # number of classes
+    n_h = 50                                # dimensions of the GloVe vectors
+
+    # Initialize parameters using Xavier initialization
+    W = np.random.randn(n_y, n_h) / np.sqrt(n_h)
+    b = np.zeros((n_y,))
+
+    # Convert Y to Y_onehot with n_y classes
+    Y_oh = convert_to_one_hot(Y, C = n_y)
+
+    # Optimization loop
+    for t in range(num_iterations):                       # Loop over the number of iterations
+        for i in range(m):                                # Loop over the training examples
+
+            ### START CODE HERE ### (鈮?4 lines of code)
+            # Average the word vectors of the words from the j'th training example
+            avg = sentence_to_avg(X[i], word_to_vec_map)
+
+            # Forward propagate the avg through the softmax layer
+            z = np.dot(W, avg) + b
+            a = softmax(z)
+
+            # Compute cost using the j'th training label's one hot representation and "A" (the output of the softmax)
+            cost = -np.sum(Y_oh[i] * np.log(a))
+            ### END CODE HERE ###
+
+            # Compute gradients
+            dz = a - Y_oh[i]
+            dW = np.dot(dz.reshape(n_y,1), avg.reshape(1, n_h))
+            db = dz
+
+            # Update parameters with Stochastic Gradient Descent
+            W = W - learning_rate * dW
+            b = b - learning_rate * db
+
+        if t % 100 == 0:
+            print("Epoch: " + str(t) + " --- cost = " + str(cost))
+            pred = predict(X, Y, W, b, word_to_vec_map)
+
+    return pred, W, b
+
+
+import numpy as np
+np.random.seed(0)
+from keras.models import Model
+from keras.layers import Dense, Input, Dropout, LSTM, Activation
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+np.random.seed(1)
+from keras.initializers import glorot_uniform
+
+
+# GRADED FUNCTION: sentences_to_indices
+def sentences_to_indices(X, word_to_index, max_len):
+    """
+    Converts an array of sentences (strings) into an array of indices corresponding to words in the sentences.
+    The output shape should be such that it can be given to `Embedding()` (described in Figure 4).
+
+    Arguments:
+    X -- array of sentences (strings), of shape (m, 1)
+    word_to_index -- a dictionary containing the each word mapped to its index
+    max_len -- maximum number of words in a sentence. You can assume every sentence in X is no longer than this.
+
+    Returns:
+    X_indices -- array of indices corresponding to words in the sentences from X, of shape (m, max_len)
+    """
+
+    m = X.shape[0]                                   # number of training examples
+
+    ### START CODE HERE ###
+    # Initialize X_indices as a numpy matrix of zeros and the correct shape
+    X_indices = np.zeros((m, max_len))
+
+    for i in range(m):                               # loop over training examples
+
+        # Convert the ith training sentence in lower case and split is into words. You should get a list of words.
+        sentence_words = X[i].lower().split()
+
+        # Initialize j to 0
+        j = 0
+
+        # Loop over the words of sentence_words
+        for w in sentence_words:
+
+            # Set the (i,j)th entry of X_indices to the index of the correct word.
+            X_indices[i, j] = word_to_index[w]
+            # Increment j to j + 1
+            j = j + 1
+
+    ### END CODE HERE ###
+
+    return X_indices
+
+
+# GRADED FUNCTION: pretrained_embedding_layer
+def pretrained_embedding_layer(word_to_vec_map, word_to_index):
+    """
+    Creates a Keras Embedding() layer and loads in pre-trained GloVe 50-dimensional vectors.
+
+    Arguments:
+    word_to_vec_map -- dictionary mapping words to their GloVe vector representation.
+    word_to_index -- dictionary mapping from words to their indices in the vocabulary (400,001 words)
+
+    Returns:
+    embedding_layer -- pretrained layer Keras instance
+    """
+
+    vocab_len = len(word_to_index) + 1                  # adding 1 to fit Keras embedding (requirement)
+    emb_dim = word_to_vec_map["cucumber"].shape[0]      # define dimensionality of your GloVe word vectors (= 50)
+
+    ### START CODE HERE ###
+    # Initialize the embedding matrix as a numpy array of zeros of shape (vocab_len, dimensions of word vectors = emb_dim)
+    emb_matrix = np.zeros((vocab_len, emb_dim))
+
+    # Set each row "index" of the embedding matrix to be the word vector representation of the "index"th word of the vocabulary
+    for word, index in word_to_index.items():
+        emb_matrix[index, :] = word_to_vec_map[word]
+
+    # Define Keras embedding layer with the correct output/input sizes, make it trainable.
+    # Use Embedding(...). Make sure to set trainable=False.
+    embedding_layer = Embedding(vocab_len,emb_dim, trainable=False)
+    ### END CODE HERE ###
+
+    # Build the embedding layer, it is required before setting the weights of the embedding layer. Do not modify the "None".
+    embedding_layer.build((None,))
+
+    # Set the weights of the embedding layer to the embedding matrix. Your layer is now pretrained.
+    embedding_layer.set_weights([emb_matrix])
+
+    return embedding_layer
+
+
+# GRADED FUNCTION: Emojify_V2
+def Emojify_V2(input_shape, word_to_vec_map, word_to_index):
+    """
+    Function creating the Emojify-v2 model's graph.
+
+    Arguments:
+    input_shape -- shape of the input, usually (max_len,)
+    word_to_vec_map -- dictionary mapping every word in a vocabulary into its 50-dimensional vector representation
+    word_to_index -- dictionary mapping from words to their indices in the vocabulary (400,001 words)
+
+    Returns:
+    model -- a model instance in Keras
+    """
+
+    ### START CODE HERE ###
+    # Define sentence_indices as the input of the graph, it should be of shape input_shape and dtype 'int32' (as it contains indices).
+    sentence_indices = Input(shape=input_shape, dtype = 'int32')
+
+    # Create the embedding layer pretrained with GloVe Vectors (鈮? line)
+    embedding_layer = pretrained_embedding_layer(word_to_vec_map, word_to_index)
+
+    # Propagate sentence_indices through your embedding layer, you get back the embeddings
+    embeddings = embedding_layer(sentence_indices)
+
+    # Propagate the embeddings through an LSTM layer with 128-dimensional hidden state
+    # Be careful, the returned output should be a batch of sequences.
+    X = LSTM(128, return_sequences=True)(embeddings)
+    # Add dropout with a probability of 0.5
+    X = Dropout(0.5)(X)
+    # Propagate X trough another LSTM layer with 128-dimensional hidden state
+    # Be careful, the returned output should be a single hidden state, not a batch of sequences.
+    X = LSTM(128, return_sequences=False)(X)
+    # Add dropout with a probability of 0.5
+    X = Dropout(0.5)(X)
+    # Propagate X through a Dense layer with softmax activation to get back a batch of 5-dimensional vectors.
+    X = Dense(5, activation='softmax')(X)
+    # Add a softmax activation
+    X = Activation('softmax')(X)
+
+    # Create Model instance which converts sentence_indices into X.
+    model = Model(inputs=sentence_indices ,outputs=X)
+
+    ### END CODE HERE ###
+
+    return model
+
+
+model = Emojify_V2((maxLen,), word_to_vec_map, word_to_index)
+model.summary()
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+X_train_indices = sentences_to_indices(X_train, word_to_index, maxLen)
+Y_train_oh = convert_to_one_hot(Y_train, C = 5)
+
+model.fit(X_train_indices, Y_train_oh, epochs = 50, batch_size = 32, shuffle=True)
+
+X_test_indices = sentences_to_indices(X_test, word_to_index, max_len = maxLen)
+Y_test_oh = convert_to_one_hot(Y_test, C = 5)
+loss, acc = model.evaluate(X_test_indices, Y_test_oh)
+print()
+print("Test accuracy = ", acc)
